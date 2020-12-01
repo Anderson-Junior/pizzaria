29f09bad6bcedea956538d57eef81aab0637fe39
Anderson-Junior <ander.lemos.jr@gmail.com>
2020-12-01 13:23:45 -0300

    Criando layout responsivo mais correções

--- _scripts/script.js
+++ _scripts/script.js
@@ -164,37 +164,46 @@ $('footer').waypoint(function(direcao) {
     offset:'650px;'
 })
 
-$('header h1').waypoint(function(direcao){
-    if(direcao == 'down'){
+$('header h1').waypoint(function(direcao) {
+    if ( direcao == 'down') {
         $('nav').addClass('navEstatico');
         $('nav div').addClass('logoEstatico');
-        $('nav ul').addClass('menuEstatico0');
-        $('nav ul li a').addClass('itensEstatico');
-        $('av ion-icon').addClass('iconeEstatico');
-    }else{
+        $('nav ul').addClass('menuEstatico');
+        $('nav ul li a').addClass('itemEstatico');
+        $('nav ion-icon').addClass('iconeEstatico');
+    } else {
         $('nav').removeClass('navEstatico');
-        $('nav div').removeClass('logoEstatico')
-        $('nav ul').removeClass('menuEstatico0');
-        $('nav ul li a').removeClass('itensEstatico');
-    }   
+        $('nav div').removeClass('logoEstatico');
+        $('nav ul').removeClass('menuEstatico');
+        $('nav ul li a').removeClass('itemEstatico'); 
+        $('nav ion-icon').removeClass('iconeEstatico');     
+    }
 },{
     offset:'200px;'
-})
+});
 
 // acoes de navegacao
-
-$('nav ul li::eq(0)').click(function(){
-    $('html, body').animate({scrollTop:$('section#domingo-perfeito').offset().top},1000);
+$('nav div').click(function() {
+    $('html,body').animate({scrollTop:$('header').offset().top},1000);
 })
 
-$('nav ul li::eq(1)').click(function(){
-    $('html, body').animate({scrollTop:$('section#pizza').offset().top},1000);
+$('nav ul li:eq(0)').click(function() {
+    $('html,body').animate({scrollTop:$('section#domingo-perfeito').offset().top},1000);
 })
 
-$('nav ul li::eq(1)').click(function(){
-    $('html, body').animate({scrollTop:$('section#testemunha').offset().top},1000);
+$('nav ul li:eq(1)').click(function() {
+    $('html,body').animate({scrollTop:$('section#pizza').offset().top},1000);
 })
 
-$('nav ul li::eq(1)').click(function(){
-    $('html, body').animate({scrollTop:$('section#plano-principal').offset().top},1000);
+$('nav ul li:eq(2)').click(function() {
+    $('html,body').animate({scrollTop:$('section#testemunha').offset().top},1000);
+})
+
+$('nav ul li:eq(3)').click(function() {
+    $('html,body').animate({scrollTop:$('section#plano-principal').offset().top},1000);
+})
+
+$('nav ul#icone-menu-mobile').click(function(){
+    $('nav ul#esquerda').css('display', 'flex');
+    $('nav ul#direita').css('display', 'flex');
 })
